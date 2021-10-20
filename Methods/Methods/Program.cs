2200f68ff0a32d6f6d740bd6edea131ee7967cb4
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2021-10-20 12:08:50 +0530

    Add files via upload

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            circle ObjCircle = new circle(); // value type parameter 
+            int number = 10;
+            int ans = ObjCircle.aera(number);
+            Console.WriteLine("Aera Of Circle is " + ans);
+
+        program ObjProgram = new program();
+         int num1 = 10;
+         int num2 = 20;
+
+        ObjProgram.display(num1, num2);//num3 is Optional Parameter
+
+            Console.Read();
+        }
+    }
+    class circle
+
+        {
+        public int aera(int radius)
+        {
+            int answer = radius * radius * (22 / 7);
+            return answer;
+        }
+        }
+    class program
+    {
+        public void display(int num1,int num2,int num3=30)
+        {
+            Console.WriteLine("num1 : {0}", num1);
+            Console.WriteLine("num2 : {0}", num2);
+            Console.WriteLine("num3 : {0}", num3);
+        }
+
+    }
+}
